c02d6d524168f0d0e5ec5b21792577ad80d20769
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-08-09 14:46:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ const { Octokit } = require('@octokit/rest');
 async function fetchDiscussionContent() {
   try {
     const octokit = new Octokit({
-      auth: process.env.GITHUB_TOKEN,
+      auth: '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',
     });
 
-    const discussionNumber = process.env.INPUT_DISCUSSION_NUMBER;
+    // Access the discussion number from the event payload
+    const discussionNumber = github.event.discussion.number;
 
     // Fetch the content of the discussion
     const discussion = await octokit.rest.discussions.get({
-      owner: context.repo['kiran-godse'], 
-      repo: context.repo['Demo_Repo'],   
+      owner: 'kiran-godse',  
+      repo: 'Demo_Repo',   
+      discussion_number: discussionNumber,
     });
 
     // Print the content of the discussion
