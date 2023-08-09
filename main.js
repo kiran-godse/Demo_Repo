21500a66e364f50b5cbd4fa5539e13c54601cbf9
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-08-09 15:25:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,25 +3,28 @@ const { Octokit } = require('@octokit/rest');
 async function fetchDiscussionContent() {
   try {
     const octokit = new Octokit({
-      auth: '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',
+       auth: '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',
     });
 
-    // Access the discussion number from the event payload
-    const discussionNumber = github.event.discussion.number;
+    const discussionNumber = process.env.INPUT_DISCUSSION_NUMBER;
 
-    // Fetch the content of the discussion
     const discussion = await octokit.rest.discussions.get({
       owner: 'kiran-godse',  
       repo: 'Demo_Repo',   
       discussion_number: discussionNumber,
     });
 
-    // Print the content of the discussion
-    console.log(`Discussion Content:\n${discussion.data.body}`);
+    const discussionContent = discussion.data.body;
+    console.log(`Discussion Content:\n${discussionContent}`);
+
+    // Set the discussion content as an output
+    console.log(`::set-output name=discussion_content::${discussionContent}`);
   } catch (error) {
     console.error('Error:', error);
+    process.exit(1);
   }
 }
 
 // Call the async function to initiate the action logic
 fetchDiscussionContent();
+
