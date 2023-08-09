e4dc476c118393d9a86bbbe411d5d5f27944b1b9
Kiran P Godse <kpgodse@gmail.com>
2023-08-09 16:42:10 +0530

    files name changed

---
+++ main.js
@@ -0,0 +1,30 @@
+const { Octokit } = require('@octokit/rest');
+
+async function fetchDiscussionContent() {
+  try {
+    const octokit = new Octokit({
+       auth: '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',
+    });
+
+    const discussionNumber = process.env.INPUT_DISCUSSION_NUMBER;
+
+    const discussion = await octokit.rest.discussions.get({
+      owner: 'kiran-godse',  
+      repo: 'Demo_Repo',   
+      discussion_number: discussionNumber,
+    });
+
+    const discussionContent = discussion.data.body;
+    console.log(`Discussion Content:\n${discussionContent}`);
+
+    // Set the discussion content as an output
+    console.log(`::set-output name=discussion_content::${discussionContent}`);
+  } catch (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+}
+
+// Call the async function to initiate the action logic
+fetchDiscussionContent();
+
