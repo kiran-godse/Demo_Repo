84f12e47b9112281e1f7a3a373b8086b4e1d0040
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-08-10 09:39:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const { Octokit } = require('@octokit/rest');
 
 async function fetchDiscussionContent() {
   try {
-    const token = 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
-    const octokit = github.getOctokit(token);
+    const octokit = new Octokit({
+       auth: process.env.MY_PERSONAL_TOKEN,
+    });
 
-    const discussionNumber = core.getInput('discussion_number');
+    const discussionNumber = process.env.INPUT_DISCUSSION_NUMBER;
 
     const discussion = await octokit.rest.discussions.get({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
+      owner: 'kiran-godse',  
+      repo: 'Demo_Repo',   
       discussion_number: discussionNumber,
     });
 
     const discussionContent = discussion.data.body;
     console.log(`Discussion Content:\n${discussionContent}`);
 
-    core.setOutput('discussion_content', discussionContent);
+    // Set the discussion content as an output
+    console.log(`::set-output name=discussion_content::${discussionContent}`);
   } catch (error) {
-    core.setFailed(`Error: ${error.message}`);
+    console.error('Error:', error);
+    process.exit(1);
   }
 }
 
